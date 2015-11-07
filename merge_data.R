517cf8b708673ad0bd54ec3d15061e66ce35bd34
s-kwak <singang.kwak@gmail.com>
2015-11-07 01:01:10 -0600

    new method of reading in with apply

--- merge_data.R
+++ merge_data.R
@@ -5,16 +5,26 @@ library(ggplot2)
 data_years <- list.files(pattern = 'MERGED') %>%
   substr(7, 10)
 
-# reads in all of the years of data
+file_names <- list.files(pattern = 'MERGED')
+
+
+
+## reads in all of the years of data
+# for (i in 1:length(data_years)){
+# assign(paste('cohort', data_years[i], sep = '_'),
+#        read.csv(list.files(pattern = data_years[i]), header = TRUE, stringsAsFactors = FALSE,
+#                 na.strings = c('NULL', 'PrivacySuppressed')))
+# }
+
+# better way of reading in using lapply
+data <- lapply(file_names, read.csv, header = TRUE, stringsAsFactors = FALSE,
+               na.strings = c('NULL', 'PrivacySuppressed'))
+
+#then add cohort year for each element of list
 for (i in 1:length(data_years)){
-assign(paste('cohort', data_years[i], sep = '_'),
-       read.csv(list.files(pattern = data_years[i]), header = TRUE, stringsAsFactors = FALSE,
-                na.strings = c('NULL', 'PrivacySuppressed')))
+data[[i]] <- data[[i]] %>%
+  mutate(COHORT = data_years[i])
+
+names(data[[i]])[1] <- 'UNITID'
 }
 
-# then update each of those
-chrt_dfs <- lapply(ls(pattern = 'cohort_'), get)
-
-
-?ls
-list
